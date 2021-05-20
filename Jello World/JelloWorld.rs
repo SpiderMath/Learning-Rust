b87f5bad75115edcec5f46b31f62a132b5edc4b7
SpiderMath <71999854+SpiderMath@users.noreply.github.com>
2021-05-20 09:44:51 +0530

    Learnt printing Jello World, feeling hungry :yum:

---
+++ Jello World/JelloWorld.rs
@@ -0,0 +1,7 @@
+// Main is the main function, which is run all the other functions can be called from here, fn is for declaring the function
+fn main() {
+    println!("Jello World!");
+    // println! -> Prints to the console on the next Line, the ! is important, since it is a 'macro' [What's that tho]
+    print!("Jell World! x2!");
+    // print! -> Prints to the console, but does not move the curson to the next line, ! is important (again)
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Learning Rust
- 
+
+I'm simply learning Rust, if you get anything from this then go ahead  
+The License is MIT by the way :smiling_imp:
